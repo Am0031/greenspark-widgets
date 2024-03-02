2cf409e4bc9c02773aca33af7b04756a0ecb57ec
Amelie Pira <amelie.pira@gmail.com>
2024-03-02 23:35:13 +0000

    removed commented out

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import Routes from "./routes";
 import "./App.css";
 import ThemeCustomisation from "./themes";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { lime, purple } from "@mui/material/colors";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#3B755F",
-    },
-    secondary: purple,
-  },
-});
 
 function App() {
   return (
-    // <ThemeProvider theme={theme}>
     <ThemeCustomisation>
       <main className="App">
         <Routes />
       </main>
     </ThemeCustomisation>
-    // </ThemeProvider>
   );
 }
 
